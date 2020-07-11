0b24c0a350659b0d902d339b52cf10fa74e9c292
WilliamDann <wcdann1@gmail.com>
2020-07-10 22:05:07 -0700

    Added hash funcs #3

--- src/UserData.js
+++ src/UserData.js
@@ -1,10 +1,28 @@
 // UserData.js
 // class for storing user information on the server
+var md5 = require('md5');
 
 class UserData {
     constructor(username, passwordHash, email) {
         this.username = username;
         this.passwordHash = passwordHash;
         this.email = email;
+    }
+
+    /**
+     * Check if a password is correct
+     * @param {String} hashed The stored hashed password
+     * @param {String} password The password to check
+     */
+    static checkPassword(hashed, password) {
+        return md5(password) == hashed;
+    }
+
+    /**
+     * hash a password for storage in db
+     * @param {String} password the password to hash
+     */
+    static hashPassowrd(password) {
+        return md5(password);
     }
 } module.exports = UserData;
